8415b8dd5ab189a9db323971c65b67010b094f9b
ram <ramachandran@knetworks.co.in>
2017-03-06 16:25:26 +0530

    .so files integrated

--- android/build/generated/source/r/debug/mil/nga/geopackage/R.java
+++ android/build/generated/source/r/debug/mil/nga/geopackage/R.java
@@ -8,48 +8,48 @@ package mil.nga.geopackage;
 
 public final class R {
 	public static final class bool {
-		public static final int feature_tiles_polygon_fill = 0x7f090007;
-		public static final int manager_validation_import_header = 0x7f090008;
-		public static final int manager_validation_import_integrity = 0x7f090009;
-		public static final int manager_validation_open_header = 0x7f09000a;
-		public static final int manager_validation_open_integrity = 0x7f09000b;
-		public static final int number_features_tile_border_draw = 0x7f09000c;
-		public static final int number_features_tile_circle_draw = 0x7f09000d;
-		public static final int number_features_tile_circle_fill_draw = 0x7f09000e;
-		public static final int number_features_tile_fill_draw = 0x7f09000f;
-		public static final int number_features_tile_unindexed_draw = 0x7f090010;
+		public static int feature_tiles_polygon_fill = 0x7f090007;
+		public static int manager_validation_import_header = 0x7f090008;
+		public static int manager_validation_import_integrity = 0x7f090009;
+		public static int manager_validation_open_header = 0x7f09000a;
+		public static int manager_validation_open_integrity = 0x7f09000b;
+		public static int number_features_tile_border_draw = 0x7f09000c;
+		public static int number_features_tile_circle_draw = 0x7f09000d;
+		public static int number_features_tile_circle_fill_draw = 0x7f09000e;
+		public static int number_features_tile_fill_draw = 0x7f09000f;
+		public static int number_features_tile_unindexed_draw = 0x7f090010;
 	}
 	public static final class color {
-		public static final int number_features_tile_border_color = 0x7f0b0029;
-		public static final int number_features_tile_circle_color = 0x7f0b002a;
-		public static final int number_features_tile_circle_fill_color = 0x7f0b002b;
-		public static final int number_features_tile_fill_color = 0x7f0b002c;
-		public static final int number_features_tile_text_color = 0x7f0b002d;
+		public static int number_features_tile_border_color = 0x7f0b0029;
+		public static int number_features_tile_circle_color = 0x7f0b002a;
+		public static int number_features_tile_circle_fill_color = 0x7f0b002b;
+		public static int number_features_tile_fill_color = 0x7f0b002c;
+		public static int number_features_tile_text_color = 0x7f0b002d;
 	}
 	public static final class dimen {
-		public static final int number_features_tile_border_stroke_width = 0x7f070049;
-		public static final int number_features_tile_circle_padding_percentage = 0x7f07004a;
-		public static final int number_features_tile_circle_stroke_width = 0x7f07004b;
-		public static final int number_features_tile_text_size = 0x7f07004c;
+		public static int number_features_tile_border_stroke_width = 0x7f070049;
+		public static int number_features_tile_circle_padding_percentage = 0x7f07004a;
+		public static int number_features_tile_circle_stroke_width = 0x7f07004b;
+		public static int number_features_tile_text_size = 0x7f07004c;
 	}
 	public static final class integer {
-		public static final int feature_tiles_height = 0x7f0a0004;
-		public static final int feature_tiles_polygon_fill_alpha = 0x7f0a0005;
-		public static final int feature_tiles_width = 0x7f0a0006;
+		public static int feature_tiles_height = 0x7f0a0004;
+		public static int feature_tiles_polygon_fill_alpha = 0x7f0a0005;
+		public static int feature_tiles_width = 0x7f0a0006;
 	}
 	public static final class string {
-		public static final int app_name = 0x7f06001a;
-		public static final int feature_tiles_compress_format = 0x7f060027;
-		public static final int feature_tiles_line_stroke_width = 0x7f060028;
-		public static final int feature_tiles_point_radius = 0x7f060029;
-		public static final int feature_tiles_polygon_stroke_width = 0x7f06002a;
-		public static final int geopackage_db_rollback_suffix = 0x7f06002b;
-		public static final int tile_generator_variable_max_lat = 0x7f06002c;
-		public static final int tile_generator_variable_max_lon = 0x7f06002d;
-		public static final int tile_generator_variable_min_lat = 0x7f06002e;
-		public static final int tile_generator_variable_min_lon = 0x7f06002f;
-		public static final int tile_generator_variable_x = 0x7f060030;
-		public static final int tile_generator_variable_y = 0x7f060031;
-		public static final int tile_generator_variable_z = 0x7f060032;
+		public static int app_name = 0x7f06001a;
+		public static int feature_tiles_compress_format = 0x7f060027;
+		public static int feature_tiles_line_stroke_width = 0x7f060028;
+		public static int feature_tiles_point_radius = 0x7f060029;
+		public static int feature_tiles_polygon_stroke_width = 0x7f06002a;
+		public static int geopackage_db_rollback_suffix = 0x7f06002b;
+		public static int tile_generator_variable_max_lat = 0x7f06002c;
+		public static int tile_generator_variable_max_lon = 0x7f06002d;
+		public static int tile_generator_variable_min_lat = 0x7f06002e;
+		public static int tile_generator_variable_min_lon = 0x7f06002f;
+		public static int tile_generator_variable_x = 0x7f060030;
+		public static int tile_generator_variable_y = 0x7f060031;
+		public static int tile_generator_variable_z = 0x7f060032;
 	}
 }
